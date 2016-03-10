73d8c18bcdc83bf9a15ddd2d4ad012b0c9d31b20
Quan Vu <nongdenchet8435848@gmail.com>
2016-03-10 19:52:55 +0700

    fix random picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ require 'ffaker'
 
 100.times do |index|
   Photo.create(username: "username-#{index}", caption: FFaker::Name.name,
-               url: "http://lorempixel.com/400/400/", likes_count: index)
+               url: "http://lorempixel.com/400/400/sports/#{rand(10) + 1}", likes_count: index)
 end
 
 Photo.all.each do |photo|
-  rand(100).times do |index|
+  rand(10).times do |index|
     photo.comments << Comment.new(name: "username-#{index}", description: FFaker::CompanyIT.name)
   end
 end
